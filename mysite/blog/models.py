0b54ef832cabd498a48e6979ea1c029a71d1dbe3
Marcelo Gutierrez <mmgc84@gmail.com>
2015-03-18 10:32:59 -0600

    Created Post & Comment models

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    post_title = models.CharField(max_length=None)
+    post_text = models.TextField()
+    pub_date = models.DateTimeField('date_published')
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post)
+    comment_text = models.TextField()
