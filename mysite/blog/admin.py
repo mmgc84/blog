7eb8c4ff68c0291efe5aec135a7bd3659bcb1b51
Marcelo Gutierrez <mmgc84@gmail.com>
2015-03-18 16:00:48 -0600

    Customize Post admin page

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from blog.models import Post, Comment
+
+
 # Register your models here.
+class CommentInline(admin.StackedInline):
+    model = Comment
+    extra = 0
+
+
+class PostAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Posts', {'fields': ['post_title', 'post_text', 'pub_date']}),
+    ]
+    inlines = [CommentInline]
+
+admin.site.register(Post, PostAdmin)
