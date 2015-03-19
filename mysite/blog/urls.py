b902bf4ee3c34810fa0bdc70f43c3f7d0eaf0c77
Marcelo Gutierrez <mmgc84@gmail.com>
2015-03-19 08:22:48 -0600

    Added blog detail view placeholder

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,4 +4,6 @@ from blog import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
+                       # ex: /polls/5/
+                       url(r'^(?P<post_id>\d+)/$', views.detail, name='detail'),
                        )
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the blog index.")
+
+
+def detail(request, post_id):
+    return HttpResponse("You're looking at post %s." % post_id)
